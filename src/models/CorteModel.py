07d30e13a50e6adaee81d93747e27b761ff9fff8
edgardo_bolivar <82664365+EDGARDOJBOLIVAR@users.noreply.github.com>
2023-10-02 20:53:12 -0500

    clear

--- src/models/CorteModel.py
+++ src/models/CorteModel.py
@@ -1,6 +1,5 @@
 from .BaseModel import BaseModel
 from datetime import datetime
-import pytz
 
 class CorteModel(BaseModel):
     _Table = 'corte'
@@ -10,8 +9,7 @@ class CorteModel(BaseModel):
 
     @classmethod
     def insert(self):
-        bogota_tz = pytz.timezone('America/Bogota')
-        fecha_hora_actual = datetime.now(bogota_tz)
+        fecha_hora_actual = datetime.now()
         fecha_hora_formateada = fecha_hora_actual.strftime("%Y-%m-%d %H:%M:%S")
 
         query = f"""INSERT INTO {self._Table} (id, fecha) values (DEFAULT, :date)"""
